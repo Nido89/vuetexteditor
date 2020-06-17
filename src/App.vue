69dc31f4c25f5e355c551dd419fc2758eb14e618
Naveed <naveed.jalal@edu.novia.fi>
2020-06-17 20:18:26 +0000

    Texteditor with markdown!

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+   <TextEditor id="myeditor"/>
+  </div>
+</template>
+
+<script>
+import TextEditor from './components/TextEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TextEditor
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: teal;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 60px;
+}
+#myeditor{
+  background-color: white;
+}
+</style>
